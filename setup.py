ca3d6566380d6a7bee812c3342d6864b37ba976f
mizvyt <vytautas.mizgiris@anquancapital.com>
2019-11-22 12:06:52 +0800

    Improve setup.py flow

--- setup.py
+++ setup.py
@@ -19,32 +19,32 @@ ext_files = [
     "src/bloomfilter.c",
     "src/md5.c",
     "src/primetester.c",
-    "src/MurmurHash3.c"
+    "src/MurmurHash3.c",
 ]
 
-try:
-    if "--no-cython" in sys.argv:
-        sys.argv.remove("--no-cython")
-        raise ImportError()
+# Branch out based on `--no-cython` in `argv`.
+# Assume `--cython` as default to avoid having to deal with both params being there.
 
-    from Cython.Distutils import build_ext
+if "--no-cython" in sys.argv:
+    # Use the distributed `pybloomfilter.c`.
+    # Note that we let the exception bubble up if `pybloomfilter.c` doesn't exist.
+    ext_files.append("src/pybloomfilter.c")
+    sys.argv.remove("--no-cython")
+else:
+    # Cythonize `pybloomfilter.pyx`
+    try:
+        from Cython.Distutils import build_ext
+    except ModuleNotFoundError:
+        print(
+            "Cython module not found. Hint: to build pybloomfilter using the distributed "
+            "source code, run 'python setup.py install --no-cython'."    
+        )
+        sys.exit(1)
 
     ext_files.append("src/pybloomfilter.pyx")
     setup_kwargs["cmdclass"] = {"build_ext": build_ext}
-    print("Building using Cython")
-except ImportError:
-    if "--cython" in sys.argv:
-        raise
 
-    ext_files.append("src/pybloomfilter.c")
-    print("Building from source")
-
-if "--cython" in sys.argv:
-    sys.argv.remove("--cython")
-
-ext_modules = [
-    Extension("pybloomfilter", ext_files, libraries=["crypto"])
-]
+ext_modules = [Extension("pybloomfilter", ext_files)]
 
 setup(
     name="pybloomfiltermmap3",
@@ -52,12 +52,11 @@ setup(
     author="Michael Axiak, Rob Stacey, Prashant Sinha",
     author_email="prashant@noop.pw",
     url="https://github.com/prashnts/pybloomfiltermmap3",
-    description="A Bloom filter (bloomfilter) for Python 3 built on mmap",
+    description="A fast implementation of Bloom filter for Python 3 built on mmap",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT License",
     test_suite="tests.test_all",
-    install_requires=[],
     ext_modules=ext_modules,
     classifiers=[
         "Development Status :: 4 - Beta",
