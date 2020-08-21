854253356897873739f3fcc7f44d42422a433bab
Prashant Sinha <prashant@noop.pw>
2020-08-22 01:18:15 +0200

    prepare v0.5.3 for release

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ ext_modules = [Extension("pybloomfilter", ext_files)]
 
 setup(
     name="pybloomfiltermmap3",
-    version="0.5.2",
+    version="0.5.3",
     author="Prashant Sinha",
     author_email="prashant@noop.pw",
     url="https://github.com/prashnts/pybloomfiltermmap3",
--- src/pybloomfilter.pyx
+++ src/pybloomfilter.pyx
@@ -1,6 +1,6 @@
 # cython: language_level=3
 
-VERSION = (0, 5, 2)
+VERSION = (0, 5, 3)
 AUTHOR = "Michael Axiak"
 
 __VERSION__ = VERSION
